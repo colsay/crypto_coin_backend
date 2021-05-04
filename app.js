4123870faa0caee3e999343f544a0c0185f1c291
derfc <fredcheung@live.hk>
2021-05-04 22:50:55 +0800

    imgur url

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const cors = require("cors");
 const port = process.env.PORT || 8000;
 
 const app = express();
+const fileupload = require("express-fileupload");
+
+const imgur = require("imgur");
+// var bodyParser = require("body-parser");
+
+app.use(fileupload());
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 app.use(cors());
@@ -16,15 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const metadataRouter = require("./routers/MetadataRouter")(express);
 const cchTransactionRouter = require("./routers/transactionRouter")(express);
+const createRouter = require("./routers/CreateRouter")(express);
 const nftItemRouter = require("./routers/NftItemRouter")(express);
 // const nftTransactionRouter = require("./routers/NftTransactionRouter")(express);
 app.use("/", metadataRouter);
 app.use("/", nftItemRouter);
 // app.use("/", nftTransactionRouter);
 app.use("/transaction", cchTransactionRouter);
+app.use("/upload", createRouter);
 
 app.listen(port, () => {
-  console.log(`Application listening to port ${port}`);
+	console.log(`Application listening to port ${port}`);
 });
 
 module.exports.app = app;
